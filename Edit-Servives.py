c9861277a2b175d25bdba623496c0f45b365b2dc
Marilud Uribe <mariludft10@gmail.com>
2026-04-30 16:35:58 -0500

    Correccion opcion editar

---
+++ Edit-Servives.py
@@ -0,0 +1,18 @@
+
+def editar(servicios):
+    nombre = input("Nombre del servicio a editar: ")
+
+    for s in servicios:
+        if s["nombre"] == nombre:
+            print("\n✏️ Editando servicio")
+
+            s["nombre"] = input("Nuevo nombre: ")
+            s["precio"] = input("Nuevo precio: ")
+            s["tipo_evento"] = input("Nuevo tipo: ")
+            s["duracion"] = input("Nueva duración: ")
+
+            guardar(servicios)
+            print("¡Servicio actualizado!")
+            return
+
+    print("-Servicio no encontrado-")
