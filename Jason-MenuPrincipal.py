ac54e7531395e187e7f554f428f287a9cadf7c0a
Marilud Uribe <mariludft10@gmail.com>
2026-04-30 15:16:59 -0500

    Menu

--- Jason-MenuPrincipal.py
+++ Jason-MenuPrincipal.py
@@ -10,4 +10,34 @@ def cargar_datos():
 def guardar_datos(servicios):
     with open("servicios.json", "w") as archivo:
         json.dump(servicios, archivo, indent=4)
-        
+
+def mostrar_servicios(servicios):
+    for s in servicios:
+        print(s)
+
+
+def menu():
+    servicios = cargar_datos()
+
+    while True:
+        print("\n1. Ver servicios")
+        print("2. Agregar servicio")
+        print("3. Editar servicio")
+        print("4. Eliminar servicio")
+        print("5. Salir")
+
+        opcion = input("Seleccione: ")
+
+        if opcion == "1":
+            mostrar_servicios(servicios)
+        elif opcion == "2":
+            agregar_servicio(servicios)
+        elif opcion == "3":
+            editar_servicio(servicios)
+        elif opcion == "4":
+            eliminar_servicio(servicios)
+        elif opcion == "5":
+            guardar_datos(servicios)
+            break
+        else:
+            print("Opción inválida")
